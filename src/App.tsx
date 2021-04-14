6c4bd182d2b9e2211a50ca8d96d5823042996a31
SeongWoo Kang <you3667@nepirity.com>
2021-04-14 14:12:30 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
+import { useRef } from 'react';
+import useDoubleClick from 'use-double-click';
 import './App.css';
 
 function App() {
+
+  const Button22 = () => {
+    const buttonRef = useRef(null);
+
+    useDoubleClick({
+      onSingleClick: e => {
+        console.log(e, 'single click');
+      },
+      onDoubleClick: e => {
+        console.log(e, 'double click');
+      },
+      ref: buttonRef,
+      latency: 250
+    });
+
+    return <button ref={buttonRef}>Click Me</button>
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +38,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button22 />
       </header>
     </div>
   );
